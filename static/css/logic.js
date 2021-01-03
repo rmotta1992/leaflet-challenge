c2dc5094642890da023b6553cc7486ff8162466c
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2021-01-02 23:03:32 -0500

    adding working map

--- static/css/logic.js
+++ static/css/logic.js
@@ -1,5 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -38,17 +38,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "satellite-v9",
+    id: "dark-v10",
     accessToken: API_KEY
   });
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Satellite": satellite,
-    "Street Map": streetmap
+    "Street Map": streetmap,
+    "Dark Map": darkmap
   };
 
   // Create overlay object to hold our overlay layer
@@ -56,39 +56,13 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
-  d3.json(queryUrl, function(response) {
-
-    // Create a new marker cluster group
-    var markers = L.markerClusterGroup();
-  
-    // Loop through data
-    for (var i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable
-      var location = response[i].location;
-  
-      // Check for location property
-      if (location) {
-  
-        // Add a new marker to the cluster group and bind a pop-up
-        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-          .bindPopup(response[i].descriptor));
-      }
-  
-    }
-  
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
-  
-  });
-
   // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
+  var myMap = L.map("mapid", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [satellite, earthquakes]
+    layers: [streetmap, earthquakes]
   });
 
   // Create a layer control
