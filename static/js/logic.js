44dd656cea5cf4f815c83ed9e6024b5d00959b9d
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2021-01-02 23:29:23 -0500

    claening files for final submission

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,12 @@
-// Store our API endpoint inside queryUrl
+// USGS URl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
@@ -18,19 +14,17 @@ function createFeatures(earthquakeData) {
         "<p>" + "Minimum Depth(degrees): " + feature.properties.dmin) + "</p>";
   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
+  
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
 
-  // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -47,44 +41,33 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Define a baseMaps object to hold our base layers
+  
   var baseMaps = {
     "Satellite": satellite,
     "Street Map": streetmap
   };
 
-  // Create overlay object to hold our overlay layer
+ 
   var overlayMaps = {
     Earthquakes: earthquakes
   };
 
   d3.json(queryUrl, function(response) {
-
-    // Create a new marker cluster group
     var markers = L.markerClusterGroup();
-  
-    // Loop through data
     for (var i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable
       var location = response[i].location;
-  
-      // Check for location property
       if (location) {
   
-        // Add a new marker to the cluster group and bind a pop-up
         markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
           .bindPopup(response[i].descriptor));
       }
   
     }
   
-    // Add our marker cluster layer to the map
     myMap.addLayer(markers);
   
   });
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -93,9 +76,7 @@ function createMap(earthquakes) {
     layers: [satellite, earthquakes]
   });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
